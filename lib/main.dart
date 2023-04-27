8f76a6af3cd7fcf4174d99164ce298a9e9661f3d
t-fukumoto <t-fukumoto@wwwave.jp>
2023-04-27 18:26:09 +0900

    appファイル移行

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:one_on_one_assistant_app/data/repositories/local_store_provider.dart';
 
-import 'app.dart';
+import 'presentation/app.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -11,7 +11,6 @@ Future<void> main() async {
   runApp(
     ProviderScope(
       overrides: [
-        // 環境名セット
         storeProvider.overrideWithValue(box.store),
       ],
       child: const App(),
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:one_on_one_assistant_app/app.dart';
+import 'package:one_on_one_assistant_app/presentation/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
